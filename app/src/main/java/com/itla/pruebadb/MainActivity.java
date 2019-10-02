d663ede54e02b9ed3ed3515b6555b62d231b8401
perlac <perlacruz09@gmail.com>
2019-10-01 23:15:08 -0400

    agregando interfaz grafica

--- app/src/main/java/com/itla/pruebadb/MainActivity.java
+++ app/src/main/java/com/itla/pruebadb/MainActivity.java
@@ -3,6 +3,9 @@ package com.itla.pruebadb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.itla.pruebadb.entidades.Estudiante;
 import com.itla.pruebadb.repositorio.EstudianteRepositorio;
@@ -14,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        estudianteRepositorio = new EstudianteRepositorioDbImpl(this.getBaseContext());
+
+        /*estudianteRepositorio = new EstudianteRepositorioDbImpl(this.getBaseContext());
         Estudiante estl = new Estudiante();
         estl.setNombre("Juan Perez");
         estl.setMatricula("Mat01");
@@ -27,6 +31,28 @@ public class MainActivity extends AppCompatActivity {
         est2.setMatricula("Mat02");
 
         estudianteRepositorio.crear(est2);
+        */
+
+
+
+        Button button = findViewById(R.id.button2);
+        final EditText tvnombre = findViewById(R.id.editText3);
+        final EditText tvmatricula = findViewById(R.id.editText4);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                estudianteRepositorio = new EstudianteRepositorioDbImpl(v.getContext());
+                Estudiante est2 = new Estudiante();
+                est2.setNombre(tvnombre.getText().toString());
+                est2.setMatricula(tvmatricula.getText().toString());
+
+                estudianteRepositorio.crear(est2);
+                tvmatricula.setText("");
+                tvnombre.setText("");
+            }
+        });
+
     }
 
 }
